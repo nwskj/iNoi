2fc07956708c8c5f71088772f7658bb14acb1b1d
“li-peifeng” <peifeng@me.com>
2025-07-02 15:46:05 +0200

    fix-version

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 set -e
 appName="iNoi"
 builtAt="$(date +'%F %T %z')"
-gitAuthor="The OpenList Projects Contributors <inoi@peifeng.li>"
+gitAuthor="The iNoi Projects Contributors <inoi@peifeng.li>"
 gitCommit=$(git log --pretty=format:"%h" -1)
 
 githubAuthArgs=""
