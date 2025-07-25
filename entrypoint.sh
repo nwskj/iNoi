7c22617b7c3e4eafc584956dc2120ff31c04ee62
“li-peifeng” <peifeng@me.com>
2025-07-25 13:54:36 +0200

    fix-run-error

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,6 @@ else
    cp -a /opt/service/stop/aria2 /opt/service/start 2>/dev/null
   fi
 
-  chown -R ${PUID}:${PGID} /opt
-  exec su-exec ${PUID}:${PGID} runsvdir /opt/service/start
+  chown -R ${PUID}:${PGID} /opt/inoi/
+  exec su-exec ${PUID}:${PGID} ./iNoi server --no-prefix
 fi
